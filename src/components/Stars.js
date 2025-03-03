7fee8bac212ac107103a7f55900673df5d15f144
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2025-03-02 21:07:41 -0800

    Added Stars component and added to Leaderboard table

---
+++ src/components/Stars.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Stars.css';
+import blueStarIcon from '../assets/blue-star.svg';
+import redStarIcon from '../assets/red-star.svg';
+import yellowStarIcon from '../assets/yellow-star.svg';
+import greenStarIcon from '../assets/gree-star.svg';
+
+const Stars = ({ count = 42, name }) => {
+  return (
+    <div className="stars-container">
+        <img src={yellowStarIcon} alt="Yellow star" className="star-icon" />
+        <span className="star-count">{count}</span>
+        <img src={blueStarIcon} alt="Blue star" className="star-icon" />
+        <span className="star-count">{count}</span>
+        <img src={redStarIcon} alt="Red star" className="star-icon" />
+        <span className="star-count">{count}</span>
+        <img src={greenStarIcon} alt="Green star" className="star-icon" />
+        <span className="star-count">{count}</span>
+    </div>
+  );
+};
+
+export default Stars;
